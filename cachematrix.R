00cbd4c38a15b125800bac636c1f01090b9ecf64
dwhritenour <dwhritenour@outlook.com>
2016-03-21 21:22:15 -0400

    My code Assignment 2
    My code Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # create an inverted matrix
 # supply from cache if matrix exists
 
@@ -29,8 +28,6 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
-
 cacheSolve <- function(x, ...) {
   
   # get inverse of matrix if exists
@@ -49,79 +46,3 @@ cacheSolve <- function(x, ...) {
   cache
         
 }
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-# create an inverted matrix
-# supply from cache if matrix exists
-
-makeCacheMatrix <- function(x = matrix()) {
-  
-  # initialize cache variable
-  cache = NULL
-  
-  # create matrix
-  set <- function(y)
-  {
-    x <<- y
-    cache <<- NULL
-  }
-  
-  # get matrix
-  get <- function() x
-  
-  # invert matrix and put in cache
-  setinv <- function(solve) cache <<- solve
-  
-  # retrieve matrix from cache
-  getinv <- function() cache
-  
-  # return functions
-  list(set = set, get = get, setinv = setinv, getinv = getinv)
-
-}
-
-
-
-
-cacheSolve <- function(x, ...) {
-  
-  # get inverse of matrix if exists
-  cache <- x$getinv()
-  
-  # check if matrix is cached and return
-  if(!is.null(cache)) {
-    message("this matrix is cached - retrieving")
-    return(cache)
-  }
-  
-  # create inverse matrix if not cached and return
-  mdata <- x$get()
-  cache <- solve(mdata, ...)
-  x$setinv(cache)
-  cache
-        
-}
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> origin/master
